cb0c7ad6c77baedefb0d728538832793667bc808
Aritra <chatterjee.aritra@protonmail.com>
2023-09-07 02:26:52 +0530

    Buttons and links for socials and cv

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,11 @@ import Image from 'next/image'
 import React from 'react'
 import ME from "@/public/ME.jpeg"
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import {BsArrowRight, BsLinkedin} from "react-icons/bs"
+import {HiDownload} from "react-icons/hi"
+import { FaGithubSquare } from 'react-icons/fa'
+
 
 export default function Intro() {
   return (
@@ -54,6 +59,28 @@ export default function Intro() {
          </span>. My focus is{" "}
          <span className='underline'>MERN stack.</span>
       </motion.p>
+
+      {/*Buttons*/}
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+      initial={{opacity: 0, y: 100}}
+      animate={{opacity: 1, y: 0}}
+      transition={{
+          delay:0.1,
+        }}
+      >
+        <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+          Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
+        </Link>
+        <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10' href='/CV.pdf' download={true}>
+          Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+        </a>
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10' href="https://www.linkedin.com/in/aritra-chatterjee-264127235/" target='_blank'>
+          <BsLinkedin/>
+        </a>
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10' href="https://github.com/AritraChatterjee22" target='_blank'>
+          <FaGithubSquare/>
+        </a>
+      </motion.div>
     </section>
   )
 }
