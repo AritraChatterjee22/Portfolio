c596ba4a90d6bc5a7c05169b751835d342b522b2
Aritra <chatterjee.aritra@protonmail.com>
2023-09-07 17:58:27 +0530

    About section

---
+++ components/about.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React from 'react'
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
+
+
+export default function About() {
+  return (
+    <motion.section className='mb-28 max-w-[45rem] text-center leading-8'
+    initial={{opacity: 0, y: 100}}
+    animate={{opacity: 1, y: 0}}
+    transition={{delay: 0.175}}
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+      After completing my degree in{" "}
+      <span className="font-medium">Computer Science Engineering (CSE)</span>, I
+      have developed strong skills as a full-stack developer specializing in the{" "}
+      <span className="font-medium">MERN stack</span> with an internship under my belt. My passion for web
+      development fuels my drive to craft innovative and user-centered solutions.
+      With a solid foundation in both front-end and back-end technologies, I am
+      enthusiastic about contributing my expertise to a dynamic team and
+      continually expanding my knowledge in the realm of software development. I am
+      actively seeking opportunities that enable me to apply my technical
+      proficiency and contribute to the success of forward-thinking organizations.
+      </p>
+
+      <p>
+      <span className="italic">Beyond coding</span>, I find joy in reading books,
+      learning new things, and spending quality time with my cat. My pursuit of
+      knowledge extends beyond programming, as I am currently delving into{" "}
+      <span className="font-medium">history and philosophy</span>. Additionally,
+      I'm honing my skills in playing the piano, always eager to explore new
+      horizons in both technology and life experiences. I am currently in search
+      of a <span className="font-medium">full-time position</span> as a software
+      developer to embark on the next chapter of my career.
+     </p>
+    </motion.section>
+  )
+}
