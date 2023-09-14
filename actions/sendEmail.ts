5876349abcb7b5db16599b8f7cc8b3a55077e224
Aritra <chatterjee.aritra@protonmail.com>
2023-09-14 13:59:03 +0530

    server validation of email with helper function

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,25 +1,32 @@
 "use server"
 import { Resend } from "resend"
+import { validateString } from "@/lib/utils"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
+
 
 export const sendEmail = async(formData: FormData) => {
   const senderEmail = formData.get('email')
   const message = formData.get('message')
 
-  if(!message || typeof message !== "string"){
+  //server side validation
+  if(!validateString(senderEmail, 100)){
     return {
+      error: "Invalid sender email"
+    }
+  }
+  if(!validateString(message, 5000)){
+    return{
       error: "Invalid message"
     }
   }
-
 
 
   resend.emails.send({
     from: 'onboarding@resend.dev',
     to: 'chatterjee.aritra@protonmail.com',
     subject: 'Message from Portfolio website',
-    reply_to: senderEmail,
-    text: message,
+    reply_to: senderEmail as string,
+    text: message as string,
   })
 }
