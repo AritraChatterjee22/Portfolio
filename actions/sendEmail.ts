b12a9b9cde82673dcfea643db2d4f304f26be9fa
Aritra <chatterjee.aritra@protonmail.com>
2023-09-17 17:33:19 +0530

    react email and submit form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,9 @@
 "use server"
+import React from "react"
 import { Resend } from "resend"
 import { validateString } from "@/lib/utils"
 import { getErrorMessage } from "@/lib/utils"
-
+import ContactFormEmail from "@/email/contact-form-email"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -31,7 +32,10 @@ export const sendEmail = async(formData: FormData) => {
       to: 'chatterjee.aritra@protonmail.com',
       subject: 'Message from Portfolio website',
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     })
   } catch (error: unknown) {
       return {
