6295f8583f51187cdeb36817c1e05a2b6db6b90b
Aritra <chatterjee.aritra@protonmail.com>
2023-09-17 19:22:10 +0530

    theme switch toogle on click

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,12 +1,12 @@
 "use client"
 import React, { useState } from 'react'
-import { BsSun } from 'react-icons/bs'
+import { BsMoon, BsSun } from 'react-icons/bs'
 
 
 type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
-  const [theme, setTheme] = useState()<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("light");
 
   const toogleTheme = () => {
       if(theme === 'light'){
@@ -17,10 +17,16 @@ export default function ThemeSwitch() {
     }
 
   return (
-    <button>
-      <BsSun className='fixed bottom-5 right-5 bg-white w-[1.6rem] h-[1.6rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all'
-      onClick={}
-      />
+    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all'
+      onClick={toogleTheme}
+    >
+    {
+      theme === "light" ? (
+        <BsSun/>
+      ) : (
+        <BsMoon/>
+      )
+    }
     </button>
   )
 }
